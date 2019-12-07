c594d75a8e07fce4db949464374b545c79b0008c
anyarom <r0mannava@mail.ru>
2019-12-07 13:23:30 +0300

    added multithreading test in integration test

--- tests/add_task_test.go
+++ tests/add_task_test.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"tasks/keeper"
 	"testing"
+	"time"
 )
 
 // создадание интеграционный тест для запроса на создание таски
@@ -17,33 +18,42 @@ func TestAddTask(t *testing.T) {
 	// настройки логирования
 	log := zerolog.New(os.Stdout).With().Logger()
 
+	for i := 0; i < 10; i++ {
+		go performRequest(log, t)
+	}
+	time.Sleep(10 * time.Second)
+}
+
+func performRequest(log zerolog.Logger, t *testing.T) {
+
 	// создадание httpClient
 	client := http.DefaultClient
 
 	// боди в запросе
 	bodyRequest := []byte(`{
-	  "method": "POST",
-	  "address": "http://google.com",
-	  "headers": [
-		{
-		  "key": "key1",
-		  "value": "val1"
-		},
-		{
-		  "key": "key2",
-		  "value": "val2"
-		}
-	  ],
-	  "body": "таска"
-	}`)
-	body := bytes.NewReader(bodyRequest)
+			  "method": "POST",
+			  "address": "http://google.com",
+			  "headers": [
+				{
+				  "key": "key1",
+				  "value": "val1"
+				},
+				{
+				  "key": "key2",
+				  "value": "val2"
+				}
+			  ],
+			  "body": "таска"
+			}`)
 
+	body := bytes.NewReader(bodyRequest)
 	// отправление, используя созданный httpClient, запроса на сервер
 	resp, err := client.Post("http://127.0.0.1:8080/task", "application/json", body)
 	if err != nil {
 		log.Error().Caller().Err(err).Msg("")
 		t.Fail()
 	}
+
 	defer func() {
 		err := resp.Body.Close()
 		if err != nil {
@@ -66,4 +76,5 @@ func TestAddTask(t *testing.T) {
 		t.Fail()
 	}
 
+	log.Debug().Caller().Msg("success")
 }
